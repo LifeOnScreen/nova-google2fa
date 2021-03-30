77e1486fcd9432d5a1ac817901fd437e45909120
Charlie Thompson <charliethompson@netsells.co.uk>
2021-03-30 14:08:55 +0100

    Update composer and migration

--- database/migrations/2018_10_15_095425_create_user_2fa_table.php
+++ database/migrations/2018_10_15_095425_create_user_2fa_table.php
@@ -15,15 +15,11 @@ class CreateUser2faTable extends Migration
     {
         Schema::create('user_2fa', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('user_id');
+            $table->foreignId('user_id');
             $table->boolean('google2fa_enable')->default(false);
             $table->string('google2fa_secret')->nullable();
             $table->text('recovery')->nullable();
             $table->timestamps();
-
-            $table->foreign('user_id')
-                ->references('id')
-                ->on(config('lifeonscreen2fa.tables.user'));
         });
     }
 
