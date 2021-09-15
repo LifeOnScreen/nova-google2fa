5b187fece30f19df34f5f2d9c229cae0302355f0
Matthijs Wesseling <matthijswesseling@gmail.com>
2021-09-15 10:57:54 +0200

    Use default if referer not set

--- src/Google2fa.php
+++ src/Google2fa.php
@@ -117,7 +117,7 @@ class Google2fa extends Tool
         }
 
         if (app(Google2FAAuthenticator::class)->isAuthenticated()) {
-            return response()->redirectTo($_SERVER['HTTP_REFERER']);
+            return response()->redirectTo($_SERVER['HTTP_REFERER'] ?? config('nova.path'));
         }
 
         $data['error'] = 'One time password is invalid.';
